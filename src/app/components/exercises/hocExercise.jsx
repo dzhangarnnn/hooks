8de37516128ec28eff25f7f9ba6eef0c39029bf6
dzhangarnnn <dzhangarnnn@gmail.com>
2023-02-05 15:58:42 +0700

    main

--- src/app/components/exercises/hocExercise.jsx
+++ src/app/components/exercises/hocExercise.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import CollapseWrapper from "../common/collapse";
 import Divider from "../common/divider";
-import SimpleComponent from "./simpleComponent";
-import withFunctions from "./withFunctions";
+// import SimpleComponent from "./simpleComponent";
+// import withFunctions from "./withFunctions";
 
 const HocExercise = () => {
-    const ComponentWithHoc = withFunctions(SimpleComponent);
+    // const ComponentWithHoc = withFunctions(SimpleComponent);
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -81,7 +81,7 @@ const HocExercise = () => {
                 страницы
             </p>
             <Divider />
-            <ComponentWithHoc />
+            {/* <ComponentWithHoc /> */}
         </CollapseWrapper>
     );
 };
--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,18 +1,18 @@
-import React, { useRef } from "react";
+import React from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
-    const divRef = useRef();
-    const handleClick = () => {
-        divRef.current.innerText === "Блок"
-            ? (divRef.current.innerText = "text")
-            : (divRef.current.innerText = "Блок");
-        divRef.current.style.width === "60px"
-            ? (divRef.current.style.width = "80px")
-            : (divRef.current.style.width = "60px");
-        divRef.current.style.height === "40px"
-            ? (divRef.current.style.height = "150px")
-            : (divRef.current.style.height = "40px");
-    };
+    // const divRef = useRef();
+    // const handleClick = () => {
+    //     divRef.current.innerText === "Блок"
+    //         ? (divRef.current.innerText = "text")
+    //         : (divRef.current.innerText = "Блок");
+    //     divRef.current.style.width === "60px"
+    //         ? (divRef.current.style.width = "80px")
+    //         : (divRef.current.style.width = "60px");
+    //     divRef.current.style.height === "40px"
+    //         ? (divRef.current.style.height = "150px")
+    //         : (divRef.current.style.height = "40px");
+    // };
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -24,7 +24,7 @@ const UseRefExercise = () => {
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
             <div
-                ref={divRef}
+                // ref={divRef}
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
                     height: 40,
@@ -34,9 +34,9 @@ const UseRefExercise = () => {
             >
                 <small>Блок</small>
             </div>
-            <button className="btn btn-primary" onClick={handleClick}>
+            {/* <button className="btn btn-primary" onClick={handleClick}>
                 Изменить содержимое и размер блока
-            </button>
+            </button> */}
         </CollapseWrapper>
     );
 };
